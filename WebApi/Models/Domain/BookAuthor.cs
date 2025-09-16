aaaf633140514db6e52f56b47dd05bcd76328a6e
Huyen05112005 <baohuyen051105@gmail.com>
2025-09-16 16:24:38 +0700

    Add project files.

---
+++ WebApi/Models/Domain/BookAuthor.cs
@@ -0,0 +1,14 @@
+﻿namespace WebApi.Models.Domain
+{
+    public class BookAuthor
+    {
+        public int Id { get; set; }
+
+        public int BookID { get; set; }
+        public Book Book { get; set; }
+
+        public int AuthorID { get; set; }
+        public Author Author { get; set; }
+    }
+
+}
