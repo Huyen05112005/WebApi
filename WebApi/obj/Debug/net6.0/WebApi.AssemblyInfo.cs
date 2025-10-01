d709a1133e49c4187d4ee93c7da335a97a43e3b8
Huyen05112005 <baohuyen051105@gmail.com>
2025-10-01 14:44:49 +0700

    add 9

--- WebApi/obj/Debug/net6.0/WebApi.AssemblyInfo.cs
+++ WebApi/obj/Debug/net6.0/WebApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8519ba4813a521fc3b49a7b3f757f0ceb484a698")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+09999ed197d920ee745923066cfb00f2841afe2a")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
